4933857eac955e953fc103ec8df7a6a1d44f89dc
mostafa ahmed ghareeb <50140596+MagForBusiness@users.noreply.github.com>
2022-02-18 23:03:19 +0200

    y

--- build/utilites/logger.js
+++ build/utilites/logger.js
@@ -4,7 +4,7 @@ var logger = function (req, res,
 // eslint-disable-next-line @typescript-eslint/ban-types
 next) {
     var active_url = req.baseUrl;
-    console.log("".concat(active_url, " Was visited at ").concat(Date.now())); /***can use time now */
+    console.log("".concat(active_url, " Was visited  ")); /***can use time now */
     next();
 };
 exports.default = logger;
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -8,7 +8,7 @@ import supertest from 'supertest';
 //test EndPoints
 const request = supertest(app);
 describe('Test endpoint Main root responses', () => {
-  it('gets the api endpoint', async () => {
+  it('gets the api Main root endpoint', async () => {
     const response = await request.get('/api');
     expect(response.status).toBe(200);
   });
