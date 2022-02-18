60bdeb0d4f664d4981e5ee697b5f15fd2355991a
mostafa ahmed ghareeb <50140596+MagForBusiness@users.noreply.github.com>
2022-02-18 23:13:33 +0200

    w

--- src/routes/api/displyimage.ts
+++ src/routes/api/displyimage.ts
@@ -1,19 +1,9 @@
 import express from 'express';
 import logger from '../../utilites/logger';
-import sharp from 'sharp';
 
 
 const image_disply = express.Router();
-const semiTransparentRedPng = await sharp({
-  create: {
-    width: 48,
-    height: 48,
-    channels: 4,
-    background: { r: 255, g: 0, b: 0, alpha: 0.5 }
-  }
-})
-  .png()
-  .toBuffer();
+
 image_disply.get('/', logger, (req, res) => {
   res.send('image prevew');
 });
