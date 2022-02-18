ea652157d8b8a921d40649c51478376ed524bad2
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-18 20:44:51 +0200

    w

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,6 +1,5 @@
 // import myFunc from '../index';
 import app from '../index';
-import router from '../routes/index';
 import supertest from 'supertest';
 
 
@@ -9,17 +8,12 @@ import supertest from 'supertest';
 // });
 //test EndPoints
 const request = supertest(app);
-const request2 = supertest(router);
 describe('Test endpoint responses', () => {
     it('gets the api endpoint', async () => {
         const response = await request.get('/api');
         expect(response.status).toBe(200);
        
     })
-    it('gets the api (image-disply) endpoint', async () => {
-      const response2 = await request2.get('/api/image-disply');
-      expect(response2.status).toBe(200);
-     
-  }
-)});
+    
+});
 
