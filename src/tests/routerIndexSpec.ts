9998c1105af348076a5ff978610b2371a91e0ab2
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-18 21:41:56 +0200

    w

--- src/tests/routerIndexSpec.ts
+++ src/tests/routerIndexSpec.ts
@@ -5,7 +5,7 @@ import supertest from 'supertest';
 
 //test EndPoints
 const request = supertest(router);
-describe('Test endpoint responses', () => {
+xdescribe('Test endpoint Routers responses', () => {
    
     it('gets the api (image-disply) endpoint', async () => {
       const response = await request.get('/');
