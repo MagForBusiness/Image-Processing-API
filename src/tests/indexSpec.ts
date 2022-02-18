346d0397d4cd95524f65b5777beaac60d0c0951d
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-18 19:54:50 +0200

    i

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -3,9 +3,9 @@ import app from '../index';
 import supertest from 'supertest';
 
 
-it('expect myFunc(5) to equal 25', () => {
-  expect(myFunc(5)).toEqual(25);
-});
+// it('expect myFunc(5) to equal 25', () => {
+//   expect(myFunc(5)).toEqual(25);
+// });
 //test EndPoints
 const request = supertest(app);
 describe('Test endpoint responses', () => {
