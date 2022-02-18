070f8e21ef7520b34cedaee33a28b8be680e9823
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-18 20:46:55 +0200

    55

--- src/tests/routerIndexSpec.ts
+++ src/tests/routerIndexSpec.ts
@@ -1,11 +1,8 @@
-// import myFunc from '../index';
 import router from '../routes/index';
 import supertest from 'supertest';
 
 
-// it('expect myFunc(5) to equal 25', () => {
-//   expect(myFunc(5)).toEqual(25);
-// });
+
 //test EndPoints
 const request2 = supertest(router);
 describe('Test endpoint responses', () => {
