f3392fb8692c560430fb0d4622b78d7071ddc596
mostafa ahmed ghareeb <50140596+MagForBusiness@users.noreply.github.com>
2022-02-19 21:06:25 +0200

    e

--- src/routes/api/displyimage.ts
+++ src/routes/api/displyimage.ts
@@ -2,27 +2,33 @@ import express from 'express';
 import logger from '../../utilites/logger';
 import sharp from "sharp";
 
-
-// set up route
+// set up route 
 const image_disply = express.Router();
 image_disply.get('/', logger, (req, res) => {
-  res.send('image prevew');
-  // res.send(`<img src="assets/thumb/icelandwaterfall-compressd.jpg"/>`);
-
-
-});
+  //get URL Parameters 
+  //http://localhost:3000/api/image-disply?filename=icelandwaterfall&width=200&height=300
+ const filename = req.query.filename;
+ const width:number  = Number(req.query.width);
+ const height:number = Number(req.query.height);
+  
+    sharp(`assets/full/${filename}.jpg`).resize(width, height).toFile(`assets/thumb/${filename}-resize.jpg`);
+    res.send({
+    'filename': filename,
+    'width': width,
+    'height': height
+  })
+  });
 
 // convert image and display it in browser (from folder full) and make anew size image ( in folder thumb)
-const convertimage =async ()=> {
-  try {
-    await sharp("assets/full/icelandwaterfall.jpg")
-      .resize(300,300)
-      .toFile("assets/thumb/icelandwaterfall-compressd.jpg");
-  } catch (error) {
-    console.log(error);
-  }
-}
-convertimage();
+// const convertimage =async ()=> {
+//   try {
+//     sharp(`assets/full/${filename}.jpg`)
+//       .resize(300, 300)
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// convertimage();
 //display coverted image 
 
 export default image_disply;
