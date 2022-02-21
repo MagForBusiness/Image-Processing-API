c12b436439759bd181c9f3a0778fca4c72337dd2
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-21 18:41:12 +0200

    ee

--- src/tests/routerIndexSpec.ts
+++ src/tests/routerIndexSpec.ts
@@ -5,7 +5,9 @@ import supertest from 'supertest';
 const request = supertest(app);
 describe('Test endpoint Routers responses', () => {
   it('gets the api (image-disply?filename=icelandwaterfall&width=400&height=1000) endpoint', async () => {
-    const response = await request.get('/api/image-disply?filename=icelandwaterfall&width=400&height=1000');
+    const response = await request.get(
+      '/api/image-disply?filename=icelandwaterfall&width=400&height=1000'
+    );
     expect(response.status).toBe(200);
   });
 });
