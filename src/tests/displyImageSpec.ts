4746f9694acaf712acb6d5e61f52565c125821dd
mostafa ahmed ghareeb <50140596+MagForBusiness@users.noreply.github.com>
2022-02-21 14:11:05 +0200

    ww

--- src/tests/displyImageSpec.ts
+++ src/tests/displyImageSpec.ts
@@ -1,11 +1,11 @@
 import  ResizImage from '../routes/api/ResizeProcess';
 
-
 //test Resize Prosses
 
 describe('Test The Resize Process', () => {
   it('get the name of file = icelandwaterfall after process with Sharp will be icelandwaterfall-resize.jpg  ', () => {
-    expect(ResizImage('icelandwaterfall', 500, 300)).toThrow()
-    
+    expect(ResizImage('icelandwaterfall', 500, 300)).toBe(
+      'icelandwaterfall-resize.jpg'
+    );
   });
 });
